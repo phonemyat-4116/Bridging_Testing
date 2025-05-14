9cb36985bbf85bc6c3af0c9efd722cdcb971701d
phonemyat-4116 <myat57116@gmail.com>
2025-05-14 11:30:02 +0630

    First Commit

---
+++ src/main/java/org/db_test/Main.java
@@ -0,0 +1,20 @@
+package org.db_test;
+
+/**
+    This is the testing Main Class.
+    Created by : ....
+    Created Date : ....
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        // This is output
+        System.out.printf("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("i = " + i);
+        }
+    }
+}
---
+++ src/main/java/org/db_test/Revision.java
@@ -0,0 +1,12 @@
+package org.db_test;
+
+import java.util.Scanner;
+
+import static java.util.Scanner.*;
+
+public class Revision {
+    public static void main(String []args){
+        System.out.println("Hello World");
+    }
+
+}
