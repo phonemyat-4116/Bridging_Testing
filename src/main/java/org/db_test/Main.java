11e1b04497b7555653ccaaeb1b0eb91e9bfeb0a7
Phone Myat Thu <myat57116@gmail.com>
2025-05-19 16:15:01 +0630

    Database connection established.

--- src/main/java/org/db_test/Main.java
+++ src/main/java/org/db_test/Main.java
@@ -1,20 +1,46 @@
 package org.db_test;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 /**
-    This is the testing Main Class.
-    Created by : ....
-    Created Date : ....
+ * This is testing Main Class
+ * Created by Lin MyaT Thu
+ * Created data 13/5/2025
  */
 
 public class Main {
-    public static void main(String[] args) {
-        // This is output
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    private String ip = "localhost";
+    private String port = "3306";
+    private String dbname = "test_db";
+    private String username = "root";
+    private String password = "";
+
+    protected Connection get_db_connection() {
+        Connection con = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://" + ip + ":" + port + "/" + dbname, username, password);
+            System.out.println("Successful Connection");
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+        return con;
     }
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        Connection con = main.get_db_connection();
+
+        try {
+            if (con != null)
+                con.close();
+            System.out.println("Connection closed");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
 }
